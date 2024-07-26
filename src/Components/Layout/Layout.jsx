8c78fa546960d58052143b346e61d5c33a0c1652
Jyoti Joshi <jyoti.211718@ncit.edu.np>
2024-07-26 23:27:45 +0545

    Layout Design: header Footer

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -28,7 +28,15 @@ const Layout = () => {
             Logo
             {/* <img src="assets/img/aviyanLogo.png" className='' alt="Aviyan Sarathi" style={{ maxWidth: '250px', maxHeight: '150px' }} /> */}
           </div>
-          
+          {/* new 
+          <input type="checkbox" id="sidebar-active" />
+          <label for="sidebar-active" className='open-sidebar-button '>
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />
+            </svg>
+          </label>
+          <label id="overlay" for="sidebar-active"></label>
+         new end */}
 
 
           <div className="position-relative">
@@ -36,7 +44,13 @@ const Layout = () => {
             <nav id="navbar" className="navbar navbar-expand-lg  navbar-dark ">
 
               <div className="navbar-nav mr-auto mt-2 mt-lg-0">
-               
+                {/* new 
+                <label for="sidebar-active" className='close-sidebar-button'>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
+                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+                  </svg>
+                </label>
+                 end */}
 
                 <ul>
 
