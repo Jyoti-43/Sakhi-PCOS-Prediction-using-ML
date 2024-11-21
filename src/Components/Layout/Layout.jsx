49c8ddaacbf24843f24b0fc9ae8fea8a974c3c81
Jyoti Joshi <jyoti.211718@ncit.edu.np>
2024-11-21 11:20:18 +0545

    Layout page Updated: header and Footer section

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
-import "./layout.css"
-import { Link, Outlet, useRoutes } from 'react-router-dom'
-import Home from '../Home/Home'
-import Contact from '../ContactUs/ContactUs'
-import About from '../About/AboutUs'
-import Diagnosis from '../Diagnosis/Diagnosis'
-import SignUp from '../SignUp/SignUp'
+import React from "react";
+import "./layout.css";
+import { Link, Outlet, useRoutes } from "react-router-dom";
+import Home from "../Home/Home";
+import Contact from "../ContactUs/ContactUs";
+import About from "../About/AboutUs";
+import Diagnosis from "../Diagnosis/Diagnosis";
+import SignUp from "../SignUp/SignUp";
 
 const Layout = () => {
-
   //  Routine for rendering main content
-  
 
   return (
     <>
-
       {/* Header Starts Here */}
-      <header id="header" className="header d-flex align-items-center fixed-top m-0">
+      <header
+        id="header"
+        className="header d-flex align-items-center fixed-top m-0"
+      >
         <div className="container-fluid container-xl d-flex align-items-center justify-content-between ">
-
-          <div className='logo mx-0 pt-1'>
+          <div className="logo mx-0 pt-1">
             Logo
             {/* <img src="assets/img/aviyanLogo.png" className='' alt="Aviyan Sarathi" style={{ maxWidth: '250px', maxHeight: '150px' }} /> */}
           </div>
@@ -33,11 +32,8 @@ const Layout = () => {
           <label id="overlay" for="sidebar-active"></label>
          new end */}
 
-
           <div className="position-relative">
-
             <nav id="navbar" className="navbar navbar-expand-lg  navbar-dark ">
-
               <div className="navbar-nav mr-auto mt-2 mt-lg-0">
                 {/* new 
                 <label for="sidebar-active" className='close-sidebar-button'>
@@ -48,14 +44,41 @@ const Layout = () => {
                  end */}
 
                 <ul>
-
-                  <li type="none" className='nav-item'> <Link to="/" className='nav-link' >Home</Link> </li>
-                  <li type="none" className='nav-item'> <Link to="/about" className='nav-link'>About</Link> </li>
-                  <li type="none" className='nav-item'> <Link to="/contact" className='nav-link'>Contact</Link> </li>
-                  <li type="none" className='nav-item'> <Link to="/diagnosis" className='nav-link'>Diagnosis</Link></li>
-                  <li type="none" className='nav-item'> <Link to="/signup" className='nav-link'>Signup</Link></li>
+                  <li type="none" className="nav-item">
+                    {" "}
+                    <Link to="/" className="nav-link">
+                      Home
+                    </Link>{" "}
+                  </li>
+                  <li type="none" className="nav-item">
+                    {" "}
+                    <Link to="/about" className="nav-link">
+                      About
+                    </Link>{" "}
+                  </li>
+                  <li type="none" className="nav-item">
+                    {" "}
+                    <Link to="/contact" className="nav-link">
+                      Contact
+                    </Link>{" "}
+                  </li>
+                  <li type="none" className="nav-item">
+                    {" "}
+                    <Link to="/diagnosis" className="nav-link">
+                      Diagnosis
+                    </Link>
+                  </li>
+                  <li type="none" className="nav-item">
+                    {" "}
+                    <Link to="/signup" className="nav-link">
+                      Signup
+                    </Link>
+                  </li>
                   <li type="none">
-                    <Link to='/login'> <button className="btn  m-3" > Login</button></Link>
+                    <Link to="/login">
+                      {" "}
+                      <button className="btn  m-3"> Login</button>
+                    </Link>
                   </li>
                 </ul>
               </div>
@@ -66,36 +89,34 @@ const Layout = () => {
 
       {/* Main  starts here */}
       <main id="main">
-      {/*   {routing || <Home />} Render Home by default if no route matches */}
-                    <Outlet/>
+        {/*   {routing || <Home />} Render Home by default if no route matches */}
+        <Outlet />
       </main>
 
-
       {/* Footer  starts here */}
-      <footer id="footer" className="footer flex-shrink">
-
+      <footer id="footer" className="footer">
         <div className="footer-content py-3 ">
-          <div className="container ">
-
-            <div className="row g-2">
-              <div className="col-lg-2">
-                <h3 className="footer-heading mx-0"> <Link to="/about" className='text-white '>  About </Link></h3>
-                <h5 className="footer-para" >Somethings Here........
-                </h5>
-
+          <div className="footer-container ">
+            {/* <div className="row "> */}
+              <div className="footer-about col-lg-2">
+                <h3 className="footer-heading mx-0">
+                  {" "}
+                  <Link to="/about" className="text-white ">
+                    {" "}
+                    About{" "}
+                  </Link>
+                </h3>
+                <h5 className="footer-para">Somethings Here........</h5>
               </div>
 
 
-
-              <div className="col-lg-3 mx-4">
+              <div className="footer-navigation col  ">
                 <h3 className="footer-heading mx-2">Navigation</h3>
 
                 <ul className="footer-links  list-unstyled">
                   <li>
                     <Link to="/" className="d-flex align-items-center">
-
                       <div>
-
                         <span>Home</span>
                       </div>
                     </Link>
@@ -103,9 +124,7 @@ const Layout = () => {
 
                   <li>
                     <Link to="/about" className="d-flex align-items-center">
-
                       <div>
-
                         <span>About</span>
                       </div>
                     </Link>
@@ -113,9 +132,7 @@ const Layout = () => {
 
                   <li>
                     <Link to="/contact" className="d-flex align-items-center">
-
                       <div>
-
                         <span>Contact</span>
                       </div>
                     </Link>
@@ -123,18 +140,14 @@ const Layout = () => {
 
                   <li>
                     <Link to="/diagnosis" className="d-flex align-items-center">
-
                       <div>
-
                         <span>Diagnosis</span>
                       </div>
                     </Link>
                   </li>
                   <li>
                     <Link to="/signup" className="d-flex align-items-center">
-
                       <div>
-
                         <span>SignUp</span>
                       </div>
                     </Link>
@@ -142,48 +155,44 @@ const Layout = () => {
                 </ul>
               </div>
 
-              <div className="col-lg-1 col-1 mx-0 position-relative logo-column">
-                <span><h3 className="footer-heading ">Logo Here...</h3></span>
-                <img src="" className="img-fluid my-5 logo-footer" alt="Sakhi Logo" />
-
-
+              <div className=" logo-column">
+                {/* <span>
+                  <h3 className="footer-heading ">Logo Here...</h3>
+                </span> */}
+                <img
+                  src=""
+                  
+                  alt="Sakhi Logo"
+                />
               </div>
-
-
-
             </div>
           </div>
-        </div>
+        {/* </div> */}
 
         <div className="footer-legal py-2 ">
           <div className="container">
-
             <div className="row g-6 justify-content-between m-0">
               <div className="col-md-6 text-center text-md-start mb-3 mb-md-0 p-0">
                 <div className="copyright">
-                  © Copyright2024 <strong><span>Sakhi</span></strong>. All Rights Reserved
+                  © Copyright2024{" "}
+                  <strong>
+                    <span>Sakhi</span>
+                  </strong>
+                  . All Rights Reserved
                 </div>
               </div>
 
               <div className="col-md-6">
                 <div className="social-links mb-3 mb-lg-0 text-center text-md-end">
-                  <div className="credits">
-                    Designed by : Team Sakhi
-                  </div>
-
+                  <div className="credits">Designed by : Team Sakhi</div>
                 </div>
-
               </div>
-
             </div>
-
           </div>
         </div>
-
       </footer>
-
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
